6269aeb90d2e7b142c8cceb7e7c429e596beeac0
Benoit Girard <benoit.girard@databricks.com>
2021-08-06 16:35:48 -0700

    Add support for es6 modules

--- test/sample.js
+++ test/sample.js
@@ -1,26 +1,35 @@
-const constGlobalRequire = require('const-global-require');
-let letGlobalRequire = require('let-global-require');
-var varGlobalRequire = require('var-global-require');
-require('static-require');
+import { test, test2 } from "es-import";
+import { foo as NotSupported } from "rename-not-supported";
+import * as starName from "star-import";
+import DefaultImport, { NamedImport } from "defaultAndNamed";
+const constGlobalRequire = require("const-global-require");
+let letGlobalRequire = require("let-global-require");
+var varGlobalRequire = require("var-global-require");
+require("static-require");
 
 const obj = {
-    letGlobalRequire: () => {
-        function varGlobalRequire() {
-            const nonglobalRequire = require('nonglobal-require');
+  letGlobalRequire: () => {
+    async function varGlobalRequire() {
+      const nonglobalRequire = require("nonglobal-require");
+      await import("/modules/my-module.js");
 
-            constGlobalRequire.prop = 5;
-            letGlobalRequire = {};
-            const varGlobalRequire = {
-                foo: '13'
-            };
-            try {
-                obj.constGlobalRequire = varGlobalRequire;
-            } catch (letGlobalRequire) { }
-        }
+      constGlobalRequire.prop = 5;
+      letGlobalRequire = {};
+      const varGlobalRequire = {
+        foo: "13",
+      };
+      test();
+      test2();
+      rename();
+      DefaultImport();
+      try {
+        obj.constGlobalRequire = varGlobalRequire;
+      } catch (letGlobalRequire) {}
+    }
 
-        class constGlobalRequire {
-            letGlobalRequire() { }
-        }
-    },
-    varGlobalRequire() { }
-}
+    class constGlobalRequire {
+      letGlobalRequire() {}
+    }
+  },
+  varGlobalRequire() {},
+};
