3e4bf640892975132097c4ee0dff0b8d4525081b
Benoit Girard <benoit.girard@databricks.com>
2021-08-09 23:32:46 -0700

    Improve test coverage

--- test/sample.tsx
+++ test/sample.tsx
@@ -1,20 +1,26 @@
-import { test, test2 } from "es-import";
-import { foo as NotSupported } from "rename-not-supported";
-import * as starName from "star-import";
-import DefaultImport, { NamedImport } from "defaultAndNamed";
+import { test, test2 } from "./ESImport";
+import { foo as NotSupported } from "./RenameNotSupported";
+import * as starName from "./StarImport";
+import DefaultImport, { NamedImport } from "./DefaultAndNamed";
+import { ignoreFolderIndex } from "./folder";
 const constGlobalRequire = require("const-global-require");
 let letGlobalRequire = require("let-global-require");
 var varGlobalRequire = require("var-global-require");
 require("static-require");
 
+export { x } from "./a";
+
+let t1 = {};
+type t2 = typeof t1;
+
 const obj = {
-  letGlobalRequire: () => {
+  letGlobalRequire: (foo: test) => {
     async function varGlobalRequire() {
       const nonglobalRequire = require("nonglobal-require");
       await import("/modules/my-module.js");
 
       constGlobalRequire.prop = 5;
-      letGlobalRequire = {};
+      letGlobalRequire = { [test]: true };
       const varGlobalRequire = {
         foo: "13",
       };
@@ -22,6 +28,7 @@ const obj = {
       test2();
       rename();
       DefaultImport();
+      const val = (53 + 5) as test;
       try {
         obj.constGlobalRequire = varGlobalRequire;
       } catch (letGlobalRequire) {}
